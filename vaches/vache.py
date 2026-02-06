3f38bd61077d8df4e4a4015c803cdaa0fd4c0505
Ruben-Dubord <rubendubord95@gmail.com>
2026-02-06 10:22:26 +0100

    Première version de la refactorisation

--- vaches/vache.py
+++ vaches/vache.py
@@ -1,22 +1,30 @@
 from vaches.exceptions import InvalidVacheException
 class Vache:
 
+    AGE_NAISSANCE = 0
     AGE_MAX = 25
     POIDS_MAX = 1000.0
     PANSE_MAX = 50.0
-    POIDS_MIN_PANSE = 2.0
+    POIDS_PANSE_VIDE = 0.0
     RENDEMENT_RUMINATION = 0.25
     COEFFICIENT_NUTRITIONNEL = None
     NEXT_ID = 1
 
-    def __init__(self, petitNom, poids, age):
+    def __init__(self, petitNom, poids, age=AGE_NAISSANCE):
         if petitNom is None or petitNom.strip() == '':
             raise InvalidVacheException
+
+        if poids is None or poids < self.POIDS_PANSE_VIDE:
+            raise InvalidVacheException
+
+        self._age = age
+        if not (self.AGE_NAISSANCE <= self._age <= self.AGE_MAX):
+            raise InvalidVacheException
+
         else:
             self._petitNom = petitNom
-            self._age = age
             self._poids = poids
-            self._panse = self.POIDS_MIN_PANSE
+            self._panse = self.POIDS_PANSE_VIDE
             self._id = self.NEXT_ID
             self.NEXT_ID += 1
 
@@ -44,10 +52,14 @@ class Vache:
             raise InvalidVacheException
         self._age += 1
 
-    def brouter(self, quantite = 1.0, nourriture = None):
+    def brouter(self, quantite, nourriture=None):
         if quantite <= 0:
             raise InvalidVacheException
-        else:
-            self._panse += quantite
-            if self._panse > self.PANSE_MAX:
-                raise InvalidVacheException
+
+        if nourriture is not None:
+            raise InvalidVacheException
+
+        if self._panse + quantite > self.PANSE_MAX:
+            raise InvalidVacheException
+
+        self._panse += quantite
